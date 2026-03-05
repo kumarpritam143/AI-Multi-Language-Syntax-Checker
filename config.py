e8e34a3b2aa543deea62851ae78b5fd319a9fd05
PRITAM KUMAR DAS <141274451+kumarpritam143@users.noreply.github.com>
2026-03-05 17:32:39 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-NVIDIA_API_KEY = "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"
-
-BASE_URL = "https://integrate.api.nvidia.com/v1"
-
-MODEL_NAME = "meta/llama-3.1-70b-instruct"
